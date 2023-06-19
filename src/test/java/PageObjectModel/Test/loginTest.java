44689d18d5da59aedb1ea04a8d02e220078557d4
Thapelo-Sean <thapelomatji12@gmail.com>
2023-06-19 13:00:15 +0200

    Modified loginTest class

--- src/test/java/PageObjectModel/Test/loginTest.java
+++ src/test/java/PageObjectModel/Test/loginTest.java
@@ -1,21 +1,31 @@
 package PageObjectModel.Test;
 
 import PageObjectModel.Pages.loginPage;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import java.io.File;
 import java.io.IOException;
 
 public class loginTest extends testBase{
 
+    Logger logger = LogManager.getLogger("info");
+
+
+
     @Test(priority = 1)
     public void testCase1()
     {
         try
         {
+            logger.info("<<<<<Executing Testcase 1>>>>>");
             loginPage loginPage = PageFactory.initElements(testBase.driver, loginPage.class);
             loginPage.setUsername("standard_user");
             loginPage.setPassword("secret_sauce");
@@ -30,6 +40,7 @@ public class loginTest extends testBase{
         } catch (Exception e) {
             e.printStackTrace();
         }
+        logger.info("<<<<<Testcase 1 executed>>>>>");
     }
 
     @Test(priority = 2)
@@ -38,17 +49,18 @@ public class loginTest extends testBase{
         //verify login with valid username and invalid password
         try
         {
+            logger.info("<<<<<Executing Testcase 2>>>>>");
             loginPage loginPage = PageFactory.initElements(testBase.driver, loginPage.class);
-            //driver.get(baseUrl);
             loginPage.setUsername("standard_user");
             loginPage.setPassword("Invalid_Password");
             loginPage.clickLoginButton();
 
-            //capture screenshot for valid username and invalid password
             TakesScreenshot screenshot = (TakesScreenshot)driver;
             File source = screenshot.getScreenshotAs(OutputType.FILE);
             FileUtils.copyFile(source, new File("./Screenshots/SAUC-T2.png"));
+            logger.info("Screenshot for testcase 2 captured");
             driver.navigate().refresh();
+            logger.info("<<<<<Testcase 2 executed>>>>>");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -60,6 +72,7 @@ public class loginTest extends testBase{
         //verify login with invalid username and valid password
         try
         {
+            logger.info("<<<<<Executing Testcase 3");
             loginPage loginPage = PageFactory.initElements(testBase.driver, loginPage.class);
             driver.get(baseUrl);
             loginPage.setUsername("invalid_username");
@@ -71,10 +84,10 @@ public class loginTest extends testBase{
             File source = screenshot.getScreenshotAs(OutputType.FILE);
             FileUtils.copyFile(source, new File("./Screenshots/SAUC-T3.png"));
             driver.navigate().refresh();
+            logger.info("<<<<<Testcase 3 executed>>>>>");
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
     @Test(priority = 4)
     public void testCase4()
@@ -82,6 +95,7 @@ public class loginTest extends testBase{
         //verify login with valid username and empty password field
         try
         {
+            logger.info("<<<<<Executing Testcase 4>>>>>");
             loginPage loginPage = PageFactory.initElements(testBase.driver, loginPage.class);
             loginPage.setUsername("standard_user");
             loginPage.setPassword("");
@@ -91,8 +105,8 @@ public class loginTest extends testBase{
             TakesScreenshot screenshot = (TakesScreenshot)driver;
             File source = screenshot.getScreenshotAs(OutputType.FILE);
             FileUtils.copyFile(source, new File("./Screenshots/SAUC-T4.png"));
+            logger.info("<<<<<Test case 4 executed>>>>>");
             driver.navigate().refresh();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -123,6 +137,7 @@ public class loginTest extends testBase{
         //verify login with empty username field and empty password
         try
         {
+            logger.info("<<<<<Executing Test case 6>>>>>");
             loginPage loginPage = PageFactory.initElements(testBase.driver, loginPage.class);
             loginPage.setUsername("");
             loginPage.setPassword("");
@@ -132,6 +147,7 @@ public class loginTest extends testBase{
             TakesScreenshot screenshot = (TakesScreenshot)driver;
             File source = screenshot.getScreenshotAs(OutputType.FILE);
             FileUtils.copyFile(source, new File("./Screenshots/SAUC-T6.png"));
+            logger.info("<<<<<Testcase 6 executed>>>>>");
         } catch (IOException e) {
             e.printStackTrace();
         }
