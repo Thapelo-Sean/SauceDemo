7927bf10db720d4d6b7e9abde1284bac97d222e0
Thapelo-Sean <thapelomatji12@gmail.com>
2023-06-20 08:41:12 +0200

    Modified Test.XML file

--- src/test/java/PageObjectModel/Pages/checkoutPage.java
+++ src/test/java/PageObjectModel/Pages/checkoutPage.java
@@ -1,12 +1,10 @@
 package PageObjectModel.Pages;
 
-import PageObjectModel.Test.loginTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-import org.openqa.selenium.support.PageFactory;
 
 public class checkoutPage {
     WebDriver driver;
