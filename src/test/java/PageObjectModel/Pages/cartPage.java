b70e2fdf3d4d4c51395aa9fce133737eb81c8b0f
Thapelo-Sean <thapelomatji12@gmail.com>
2023-06-20 12:56:31 +0200

    Modified carPage class

--- src/test/java/PageObjectModel/Pages/cartPage.java
+++ src/test/java/PageObjectModel/Pages/cartPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class cartPage {
-    public static WebDriver driver;
+    public WebDriver driver;
     public cartPage(WebDriver driver)
     {
         this.driver = driver;
