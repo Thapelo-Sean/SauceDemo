6c9377cf9f2e58ee0e40164f27724d47420593f4
Thapelo-Sean <thapelomatji12@gmail.com>
2023-06-20 10:47:46 +0200

    Modified loginTest class

--- src/test/java/PageObjectModel/Test/LoginTest.java
+++ src/test/java/PageObjectModel/Test/LoginTest.java
@@ -11,7 +11,6 @@ import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeTest;
