61e8808c299463c4d6413f58b6668037b6d9b56d
Thapelo Sean <102145706+Thapelo-Sean@users.noreply.github.com>
2023-06-21 08:41:01 +0200

    Update LoginTest.java

--- src/test/java/PageObjectModel/Test/LoginTest.java
+++ src/test/java/PageObjectModel/Test/LoginTest.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 
 public class LoginTest extends TestBase
 {
-
     private final Logger logger = LogManager.getLogger("Info");
     public static ExtentReports extent;
     public static ExtentSparkReporter spark;
