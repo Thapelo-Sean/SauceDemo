ecb73ada8ad4133ccb82988d364b9fbb4a775bbf
Thapelo-Sean <thapelomatji12@gmail.com>
2023-06-21 10:50:42 +0200

    Modified LogoutTest class

--- src/test/java/PageObjectModel/Test/LogoutTest.java
+++ src/test/java/PageObjectModel/Test/LogoutTest.java
@@ -3,7 +3,6 @@ package PageObjectModel.Test;
 import PageObjectModel.Pages.InventoryPage;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.Status;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,7 +10,6 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import java.io.File;
 
@@ -20,15 +18,6 @@ public class LogoutTest extends TestBase
 
     private final Logger logger = LogManager.getLogger("Info");
     public static ExtentReports extent;
-    public static ExtentSparkReporter spark;
-
-    @BeforeTest
-    public void report()
-    {
-        extent = new ExtentReports();
-        spark = new ExtentSparkReporter("./Reports/TestCasesReport.html");
-        extent.attachReporter(spark);
-    }
 
     //verify logout functionality
     @Test(priority = 13)
